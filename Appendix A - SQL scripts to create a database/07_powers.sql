edc8496f7fdce1c9a25f50fbde6e1c6dcfab5068
LisettedeWilde <lisette.de.wilde1998@outlook.com>
2022-02-18 15:13:47 +0100

    Added file 07_powers.sql which adds four superpowers to the Power table and adds four entries to the SuperheroPowers table

---
+++ Appendix A - SQL scripts to create a database/07_powers.sql
@@ -0,0 +1,15 @@
+USE SuperheroesDb;
+
+INSERT INTO Power (Name, Description)
+VALUES 
+('Enhanced strength', 'The hero possesses a level of strength beyond that of the peak members of their specie'),
+('Invisibility', 'The power to render oneself unable to be seen.'),
+('Flight', 'Ability to lift off the ground, to ride air currents or to fly self-propelled through the air'),
+('Mind control', 'The power to control the minds of others');
+
+INSERT INTO SuperheroPowers (SuperheroID, PowerID)
+VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(3, 3);
