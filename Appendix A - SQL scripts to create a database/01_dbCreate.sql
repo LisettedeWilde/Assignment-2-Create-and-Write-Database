22720e6d68518e40dd7342fe3207a4f30af66560
LisettedeWilde <lisette.de.wilde1998@outlook.com>
2022-02-18 14:00:19 +0100

    Created new branch for create database

--- Appendix A - SQL scripts to create a database/01_dbCreate.sql
+++
@@ -1,1 +0,0 @@
-CREATE DATABASE SuperheroesDb
--- Appendix A - SQL scripts to create a database/03_relationshipSuperheroAssistant.sql
+++
@@ -1,7 +0,0 @@
-USE SuperheroesDb
-
-ALTER TABLE Assistant
-ADD SuperheroID int;
-
-ALTER TABLE Assistant
-ADD FOREIGN KEY (SuperheroID) REFERENCES Superhero(ID); 
