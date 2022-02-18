6846c170d9e0d85cfb29692ec008d6c07e9df254
LisettedeWilde <lisette.de.wilde1998@outlook.com>
2022-02-18 14:45:53 +0100

    Added file 05_insertSuperheroes which adds three superheroes to the superhero table

---
+++ Appendix A - SQL scripts to create a database/05_insertSuperheroes.sql
@@ -0,0 +1,7 @@
+USE SuperheroesDb;
+
+INSERT INTO Superhero (Superhero_Name, Alias, Origin)
+VALUES 
+('Spider-Man', 'Peter Parker', 'Queens'),
+('Captain America', 'Steve Rodgers', 'Brooklyn'),
+('Superman', 'Clark Kent', 'Krypton');
