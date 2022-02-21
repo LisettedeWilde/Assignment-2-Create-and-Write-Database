221303afe24f0b070e9da1e660089ad95a4398b1
LisettedeWilde <lisette.de.wilde1998@outlook.com>
2022-02-21 10:53:31 +0100

    Added Customer class which stores a model for the Customer table in the database

---
+++ ManipulateSQLServerData/ManipulateSQLServerData/Models/Customer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulateSQLServerData.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+#nullable enable
+        public string? PostalCode { get; set; }
+        public string? Phone { get; set; }
+#nullable disable
+        public string Email { get; set; }
+    }
+}
