a04db64693625a3da689e50ab175dbdce8b4303b
LisettedeWilde <lisette.de.wilde1998@outlook.com>
2022-02-21 10:23:27 +0100

    Added project ManipulateSQLServerData and added the ConnectionStringHelper class which creates a SqlConnectionStringBuilder

---
+++ ManipulateSQLServerData/ManipulateSQLServerData/Repositories/ConnectionStringHelper.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManipulateSQLServerData.Repositories
+{
+    public class ConnectionStringHelper
+    {
+        public static string GetConnectionString()
+        {
+            SqlConnectionStringBuilder connectionStringBuilder = new SqlConnectionStringBuilder();
+            connectionStringBuilder.DataSource = "CEREBRO"; // name of sql server
+            connectionStringBuilder.InitialCatalog = "Chinook";
+            connectionStringBuilder.IntegratedSecurity = true; // use windows builtin
+            connectionStringBuilder.TrustServerCertificate = true;
+            return connectionStringBuilder.ConnectionString;
+        }
+    }
+}
