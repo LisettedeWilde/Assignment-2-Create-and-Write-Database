b37b2669cbfae9f49c0302e4eac7be76951b9c0b
LisettedeWilde <lisette.de.wilde1998@outlook.com>
2022-02-23 17:20:14 +0100

    Added comments to functions in program.cs

--- ManipulateSQLServerData/ManipulateSQLServerData/Program.cs
+++ ManipulateSQLServerData/ManipulateSQLServerData/Program.cs
@@ -13,16 +13,28 @@ namespace ManipulateSQLServerData
             TestGetFavoriteGenre(repository);
         }
 
+        /// <summary>
+        /// Prints the resulting list from GetAllCustomers to the console, which for all customers contains their: id, first name, last name, country, postal code, phone number and email 
+        /// </summary>
+        /// <param name="repository"></param>
         static void TestSelectAll(ICustomerRepository repository)
         {
             PrintCustomers(repository.GetAllCustomers());
         }
-
+        
+        /// <summary>
+        /// Prints the result from GetCustomerByName to the console
+        /// </summary>
+        /// <param name="repository"></param>
         static void TestSelectByName(ICustomerRepository repository)
         {
             PrintCustomer(repository.GetCustomerByName("Tim", "Goyer"));
         }
 
+        /// <summary>
+        /// Checks whether the customer gets added correctly to the database with the method AddNewCustomer, writes a message to the console whether the addition was successful
+        /// </summary>
+        /// <param name="repository"></param>
         static void TestInsert(ICustomerRepository repository)
         {
             Customer test = new Customer()
@@ -44,6 +56,10 @@ namespace ManipulateSQLServerData
             }
         }
 
+        /// <summary>
+        /// Prints the result from GetNumberOfCustomersPerCountry to the console
+        /// </summary>
+        /// <param name="repository"></param>
         static void TestGetCountriesCount(ICustomerRepository repository)
         {
             Dictionary<string, int> result = repository.GetNumberOfCustomersPerCountry();
@@ -54,11 +70,19 @@ namespace ManipulateSQLServerData
             }
         }
 
+        /// <summary>
+        /// Prints the result of GetFavoriteGenre to the console
+        /// </summary>
+        /// <param name="repository"></param>
         static void TestGetFavoriteGenre(ICustomerRepository repository)
         {
             Console.WriteLine(repository.GetFavoriteGenre(12));
         }
 
+        /// <summary>
+        /// Loops through an IEnumerable of Customers and prints them to the console
+        /// </summary>
+        /// <param name="customers"></param>
         static void PrintCustomers(IEnumerable<Customer> customers)
         {
             foreach (Customer customer in customers)
@@ -67,6 +91,10 @@ namespace ManipulateSQLServerData
             }
         }
 
+        /// <summary>
+        /// Prints a Customer object to the console
+        /// </summary>
+        /// <param name="customer"></param>
         static void PrintCustomer(Customer customer)
         {
             Console.WriteLine($"{customer.CustomerId} {customer.FirstName} {customer.LastName} {customer.Country} {customer.PostalCode} {customer.Phone} {customer.Email}");
